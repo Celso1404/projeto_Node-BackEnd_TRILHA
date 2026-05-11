2f921d303db47bd08b2a2a892c90d7525f59f086
Celso Bitello <celsokaiiki@gmail.com>
2026-05-11 16:26:22 -0300

    N03 - Criando um Servidor Web com Node.JS

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Helloo");
+const http = require('http');
+
+let server = http.createServer((req, res)=> { //cria o servidor e guarda na variável
+    console.log('URL:', req.url); //chama a URL
+    console.log('METHOD:', req.method); //chama o método
+
+    res.end('OK');
+});
+
+server.listen(3000, '127.0.0.1', ()=> { //configura a porta e o ip para guardar o server
+    console.log('Servidor rodando'); //função de callback quando o server subir
+})
