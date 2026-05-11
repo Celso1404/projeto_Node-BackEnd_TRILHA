b62813c47b714c4b39ead1eaec0093e55b4554ec
Celso Bitello <celsokaiiki@gmail.com>
2026-05-11 16:33:05 -0300

    N04 - Detectando URL diferente

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ let server = http.createServer((req, res)=> { //cria o servidor e guarda na vari
     console.log('URL:', req.url); //chama a URL
     console.log('METHOD:', req.method); //chama o método
 
-    res.end('OK');
+    switch(req.url) { //confirmação do funcionamento do servidor e da requisição
+        case '/':
+            res.statuscode = 200;
+            res.setHeader('Content-type', 'text/html');
+            res.end('<h1>Olá</h1>');
+            break;
+        case '/users':
+            res.statuscode = 200;
+            res.setHeader('Content-type', 'application/json');
+            res.end(JSON.stringify({
+                users: 
+                [{
+                    name: 'Celso',
+                    email: 'celsoteste@gmail.com',
+                    id: 1
+                }]
+            }));
+            break; 
+    }
 });
 
 server.listen(3000, '127.0.0.1', ()=> { //configura a porta e o ip para guardar o server
