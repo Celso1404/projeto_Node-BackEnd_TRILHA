49f9b914e94975399d4b6df681b965ffcf593683
Celso Bitello <celsokaiiki@gmail.com>
2026-05-11 17:01:44 -0300

    N06 - Nodemon e Criando o servir com Express

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res)=> { //cria o servidor e guarda na variável
-    console.log('URL:', req.url); //chama a URL
-    console.log('METHOD:', req.method); //chama o método
+let app = express();
 
-    switch(req.url) { //confirmação do funcionamento do servidor e da requisição
-        case '/':
-            res.statuscode = 200;
-            res.setHeader('Content-type', 'text/html');
-            res.end('<h1>Olá</h1>');
-            break;
-        case '/users':
-            res.statuscode = 200;
-            res.setHeader('Content-type', 'application/json');
-            res.end(JSON.stringify({
-                users: 
-                [{
-                    name: 'Celso',
-                    email: 'celsoteste@gmail.com',
-                    id: 1
-                }]
-            }));
-            break; 
-    }
+app.get('/', (req, res)=> { 
+    
+    res.statuscode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.end('<h1>Olá</h1>');//cria o servidor e guarda na variável
 });
 
-server.listen(3000, '127.0.0.1', ()=> { //configura a porta e o ip para guardar o server
+app.get('/users', (req, res)=> {
+     res.statuscode = 200;
+        res.setHeader('Content-type', 'application/json');
+        res.json({ //retorna o json
+            users: 
+            [{
+                name: 'Celso',
+                email: 'celsoteste@gmail.com',
+                id: 1
+            }]
+        });
+});
+
+app.listen(3000, '127.0.0.1', ()=> { //configura a porta e o ip para guardar o server
     console.log('Servidor rodando'); //função de callback quando o server subir
 })
