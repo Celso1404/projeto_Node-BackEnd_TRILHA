f23174ff62bfb3bfbae1cd287572e4b7f5f14363
Celso Bitello <celsokaiiki@gmail.com>
2026-05-11 19:10:21 -0300

    N12 - Refatorando sistema e otimizando o código

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let app = express();
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 
-consign().include('routes').into(app); //incluí todas as rotas no app
+consign().include('routes').include('utils').into(app); //incluí todas as rotas no app
 
 app.listen(3000, '127.0.0.1', ()=> { //configura a porta e o ip para guardar o server
     console.log('Servidor rodando'); //função de callback quando o server subir
